7f2e6a0bedb9e40308088331a050a426be582a9a
Poun Sokha <123456789+Gitechcode@users.noreply.github.com>
2026-04-02 20:54:47 +0700

    feat(test): conflict code

--- app/src/main/java/com/example/bbumobileapp/MainActivity.java
+++ app/src/main/java/com/example/bbumobileapp/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
